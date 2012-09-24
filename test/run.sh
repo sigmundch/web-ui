e919e19259de9b523ff4c697a49ed5fd08908f85
Sigmund Cherem <sigmund@google.com>
2012-09-24 12:59:54 -0700

    Add conditional tests that use components internally. Also, allow to filter tests in run.sh.
    Review URL: https://chromiumcodereview.appspot.com//10970049

--- test/run.sh
+++ test/run.sh
@@ -3,6 +3,10 @@
 # for details. All rights reserved. Use of this source code is governed by a
 # BSD-style license that can be found in the LICENSE file.
 
+# Usage: call directly in the commandline as test/run.sh ensuring that you have
+# both 'dart' and 'DumpRenderTree' in your path. Filter tests by passing a
+# pattern as an argument to this script.
+
 # TODO(sigmund): replace with a real test runner
 
 # bail on error
@@ -13,13 +17,13 @@ set -e
 
 DIR=$( cd $( dirname "${BASH_SOURCE[0]}" ) && pwd )
 DART_FLAGS="--enable-type-checks --enable-asserts"
-
-for test in $DIR/*_test.dart; do
-  dart $DART_FLAGS $test
-done
+TEST_PATTERN=$1
 
 
 function show_diff {
+  echo -en "[33mExpected[0m"
+  echo -n "                                                           "
+  echo -e "[32mOutput[0m"
   diff -t -y $1 $2 | \
     sed -e "s/\(^.\{63\}\)\(\s[<]\(\s\|$\)\)\(.*\)/[31m\1[33m\2[32m\4[0m/" |\
     sed -e "s/\(^.\{63\}\)\(\s[|]\(\s\|$\)\)\(.*\)/[33m\1[33m\2[33m\4[0m/" |\
@@ -43,14 +47,22 @@ function compare {
   diff -q -s $1 $2 || show_diff $1 $2 || update $1 $2
 }
 
-for input in $DIR/data/input/*_test.html; do
-  echo -e "\nTesting $input:"
-  FILENAME=`basename $input.html`
-  dart $DART_FLAGS bin/dwc.dart $input $DIR/data/output/
-  DUMP="$DIR/data/output/$FILENAME.txt"
-  EXPECTATION="$DIR/data/expected/$FILENAME.txt"
-  DART_PACKAGE_ROOT="file://$DIR/packages/" \
-      DumpRenderTree $DIR/data/output/$FILENAME > $DUMP
+for test in $DIR/*_test.dart; do
+  if [[ ($TEST_PATTERN == "") || ($test =~ $TEST_PATTERN) ]]; then
+    dart $DART_FLAGS $test
+  fi
+done
 
-  compare $EXPECTATION $DUMP
+for input in $DIR/data/input/*_test.html; do
+  if [[ ($TEST_PATTERN == "") || ($input =~ $TEST_PATTERN) ]]; then
+    echo -e "\nTesting $input:"
+    FILENAME=`basename $input.html`
+    dart $DART_FLAGS bin/dwc.dart $input $DIR/data/output/
+    DUMP="$DIR/data/output/$FILENAME.txt"
+    EXPECTATION="$DIR/data/expected/$FILENAME.txt"
+    DART_PACKAGE_ROOT="file://$DIR/packages/" \
+        DumpRenderTree $DIR/data/output/$FILENAME > $DUMP
+
+    compare $EXPECTATION $DUMP
+  fi
 done
